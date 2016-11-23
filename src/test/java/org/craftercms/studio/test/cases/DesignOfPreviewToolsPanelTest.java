ea18ee3c6ca3c31df739051cda77004a60a9c8b1
gortizcrafter <gustavo.ortiz@rivetlogic.com>
2016-11-23 16:14:11 -0600

    Ticket 145

---
+++ src/test/java/org/craftercms/studio/test/cases/DesignOfPreviewToolsPanelTest.java
@@ -0,0 +1,113 @@
+package org.craftercms.studio.test.cases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.craftercms.studio.test.pages.HomePage;
+import org.craftercms.studio.test.pages.LoginPage;
+import org.craftercms.studio.test.pages.PreviewPage;
+import org.craftercms.studio.test.utils.FilesLocations;
+import org.craftercms.studio.test.utils.UIElementsPropertiesManager;
+import org.craftercms.studio.test.utils.WebDriverManager;
+
+/**
+ * 
+ * @author Gustavo Andrei Ortiz Alfaro
+ *
+ */
+
+public class DesignOfPreviewToolsPanelTest {
+
+	WebDriver driver;
+
+	private WebDriverManager driverManager;
+
+	private LoginPage loginPage;
+
+	private UIElementsPropertiesManager UIElementsPropertiesManager;
+
+	private HomePage homePage;
+
+	private PreviewPage previewPage;
+
+	// The following code is for the QA needs to execute the test with phantomJS
+
+	/*
+	 * @BeforeTest public void setup() throws Exception { //Set phantomjs.exe
+	 * executable file path using DesiredCapabilities. DesiredCapabilities
+	 * capability = new DesiredCapabilities();
+	 * capability.setCapability(PhantomJSDriverService.
+	 * PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+	 * "/Users/gustavoortizalfaro/Documents/workspace/phantomjs-2.1.1-macosx/bin/phantomjs"
+	 * ); driver = new PhantomJSDriver(capability);
+	 * driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); }
+	 */
+
+	// This code shows the UI and the QA can see the steps executing in real
+	// time.
+
+	@BeforeTest
+	public void beforeTest() {
+		this.driverManager = new WebDriverManager();
+		this.UIElementsPropertiesManager = new org.craftercms.studio.test.utils.UIElementsPropertiesManager(
+				FilesLocations.UIELEMENTSPROPERTIESFILEPATH);
+		this.loginPage = new LoginPage(driverManager, this.UIElementsPropertiesManager);
+		this.homePage = new HomePage(driverManager, this.UIElementsPropertiesManager);
+		this.previewPage = new PreviewPage(driverManager, this.UIElementsPropertiesManager);
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driverManager.closeConnection();
+	}
+
+	@Test(priority = 0)
+
+	public void design_preview_tools_panel() {
+
+		// login to application
+
+		loginPage.loginToCrafter("admin", "1234");
+
+		// wait for element is clickeable
+
+		homePage.getDriverManager().driverWait();
+
+		// go to dashboard page
+
+		homePage.GoToPreviewPage();
+
+		// wait for element is clickeable
+
+		homePage.getDriverManager().driverWait();
+
+		// Click on Preview Tools icon (show)
+
+		previewPage.ClickOnPreviewTools();
+
+		// Assert
+
+		WebElement previewToolsShow = driverManager.getDriver()
+				.findElement(By.cssSelector("#preview-tools-panel-container.yui-module.yui-overlay.yui-panel"));
+
+		Assert.assertTrue(previewToolsShow.isDisplayed());
+
+		// Click on Preview Tools icon (hide)
+
+		previewPage.ClickOnPreviewTools();
+
+		// Assert
+
+		WebElement previewToolsHide = driverManager.getDriver()
+				.findElement(By.cssSelector("#preview-tools-panel-container.yui-module.yui-overlay.yui-panel"));
+
+		Assert.assertFalse(previewToolsHide.isDisplayed());
+
+	}
+
+}
