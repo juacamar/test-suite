2f84d72cf36918e37c456e780ff4f141952ddd1d
GustavoOrtizAlfaro <gustavo.ortiz@craftersoftware.com>
2017-04-26 16:53:12 -0600

    Last improvements of UI automation tests.

--- src/test/java/org/craftercms/studio/test/cases/CutPasteContentTest.java
+++ src/test/java/org/craftercms/studio/test/cases/CutPasteContentTest.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.craftercms.studio.test.pages.AdminConsolePage;
 import org.craftercms.studio.test.pages.DashboardPage;
 import org.craftercms.studio.test.pages.HomePage;
 import org.craftercms.studio.test.pages.LoginPage;
@@ -38,6 +39,8 @@ public class CutPasteContentTest {
 	private PreviewPage previewPage;
 
 	private DashboardPage dashboardPage;
+	
+	private AdminConsolePage adminConsolePage;
 
 	
 
@@ -50,6 +53,8 @@ public class CutPasteContentTest {
 		this.loginPage = new LoginPage(driverManager, this.UIElementsPropertiesManager);
 		this.homePage = new HomePage(driverManager, this.UIElementsPropertiesManager);
 		this.dashboardPage = new DashboardPage(driverManager, this.UIElementsPropertiesManager);
+		this.adminConsolePage = new AdminConsolePage(driverManager, this.UIElementsPropertiesManager);
+
 
 	}
 
@@ -65,6 +70,8 @@ public class CutPasteContentTest {
 		// login to application
 
 		loginPage.loginToCrafter("admin", "admin");
+		
+		driverManager.maximizeWindow();
 
 		// wait for element is clickeable
 
@@ -83,14 +90,160 @@ public class CutPasteContentTest {
 		driverManager.getDriver().navigate().refresh();
 
 		// Show site content panel
+				driverManager.getDriver().findElement(By.xpath("/html/body/div[2]/div[1]/nav/div/div[2]/ul[1]/li/div/div[1]/a"))
+						.click();
 
-		driverManager.getDriver().findElement(By.xpath("/html/body/div[2]/div[1]/nav/div/div[2]/ul[1]/li/div/div[1]/a"))
-				.click();
+				// wait for element is clickeable
 
-		// wait for element is clickeable
+				homePage.getDriverManager().driverWait();
 
-		homePage.getDriverManager().driverWait();
+				// go to admin console page
 
+				driverManager.getDriver().findElement(By.cssSelector("#admin-console")).click();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// select content types
+				adminConsolePage.selectContentTypeOption();
+
+				// open content types
+
+				adminConsolePage.clickExistingTypeOption();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// Select the Entry content type
+
+				adminConsolePage.selectEntryContentType();
+
+				// Confirm the content type selected
+
+				adminConsolePage.confirmContentTypeSelected();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// select main content
+
+				driverManager.getDriver().findElement(By.cssSelector("#yui-gen7")).click();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// Body not required
+
+				driverManager.getDriver()
+						.findElement(By.cssSelector("div.property-wrapper:nth-child(21) > div:nth-child(2) > input")).click();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// save
+
+				adminConsolePage.saveDragAndDropProcess();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// go to dashboard
+
+				driverManager.getDriver().findElement(By.cssSelector("#cstudio-logo")).click();
+				
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// expand pages folder
+
+				dashboardPage.expandPagesTree();
+
+				// right click to see the the menu
+
+				dashboardPage.rightClickToSeeMenu();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// Select Entry Content Type
+
+				dashboardPage.clickEntryCT();
+
+				// Confirm the Content Type selected
+
+				dashboardPage.clickOKButton();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// Switch to the iframe
+				driverManager.getDriver().switchTo().defaultContent();
+				driverManager.getDriver().switchTo()
+						.frame(driverManager.getDriver().findElement(By.cssSelector(".studio-ice-dialog > .bd iframe")));
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// Set basics fields of the new content created
+
+				dashboardPage.setBasicFieldsOfNewContent("Test1", "Testing1");
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// Expand all fields
+
+				//driverManager.getDriver().findElement(By.cssSelector("#cstudio-form-expand-all")).click();
+
+				// Set Main Content
+
+				//dashboardPage.setMetadataFields("title", "keywords");
+				
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+			
+
+				
+				// Set the title of main content
+				
+				driverManager.getDriver().findElement(By.cssSelector("#title > div > input")).sendKeys("MainTitle");
+				
+				// click necessary to validate all fields required
+				
+				driverManager.getDriver().findElement(By.cssSelector("#cstudio-form-expand-all")).click();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+				
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// save and close
+
+				driverManager.getDriver().findElement(By.id("cstudioSaveAndClose")).click();
+
+				// wait for element is clickeable
+
+				homePage.getDriverManager().driverWait();
+
+				// Switch back to the dashboard page
+
+				driverManager.getDriver().switchTo().defaultContent();
+				
 		// expand pages folder
 
 		dashboardPage.expandPagesTree();
@@ -137,7 +290,7 @@ public class CutPasteContentTest {
 
 		// Right click and paste content.
 
-		dashboardPage.rightClickToPasteOption();
+		dashboardPage.rightClickToPasteOption2();
 
 		// reload page
 
@@ -147,7 +300,7 @@ public class CutPasteContentTest {
 
 		homePage.getDriverManager().driverWait();
 		
-		driverManager.getDriver().findElement(By.cssSelector("#ygtvt4 > a:nth-child(1)")).click();
+		//driverManager.getDriver().findElement(By.cssSelector("#ygtvt4 > a:nth-child(1)")).click();
 		
 		// wait for element
 
@@ -155,9 +308,9 @@ public class CutPasteContentTest {
 
 		// Assert of the content copied
 
-		String contentCopied = driverManager.getDriver().findElement(By.cssSelector("#ygtvtableel5 > tbody > tr"))
+		String contentCopied = driverManager.getDriver().findElement(By.cssSelector("#MyRecentActivity-tbody > tr > td.urlCol"))
 				.getText();
-		Assert.assertEquals(contentCopied, "  crafter-component.xml *");
+		Assert.assertEquals(contentCopied, "/addnewfolder/test1");
 
 
 	}
