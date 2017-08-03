c779275248b3e32a8fe7e2abc0aa75f902024d1f
GustavoOrtizAlfaro <gustavo.ortiz@craftersoftware.com>
2017-08-03 11:24:54 -0600

    update files location

--- src/test/java/org/craftercms/studio/test/utils/FilesLocations.java
+++ src/test/java/org/craftercms/studio/test/utils/FilesLocations.java
@@ -2,8 +2,8 @@ package org.craftercms.studio.test.utils;
 
 public interface FilesLocations {
 
- public static String UIELEMENTSPROPERTIESFILEPATH = "/Users/gustavoortizalfaro/Documents/workspace/studio-test-suite/src/test/resources/SharedUIElements.properties";
- public static String CONSTANTSPROPERTIESFILEPATH = "/Users/gustavoortizalfaro/Documents/workspace/studio-test-suite/src/test/resources/ExecutionConstants.properties";
- public static String DATAOFTHEEXECUTIONPATH = "/Users/gustavoortizalfaro/Documents/workspace/studio-test-suite/src/test/resources/DataOfTheExecution.properties";
+ public static String UIELEMENTSPROPERTIESFILEPATH = "./src/test/resources/SharedUIElements.properties";
+ public static String CONSTANTSPROPERTIESFILEPATH = "./src/test/resources/ExecutionConstants.properties";
+ public static String DATAOFTHEEXECUTIONPATH = "./src/test/resources/DataOfTheExecution.properties";
 
 }
