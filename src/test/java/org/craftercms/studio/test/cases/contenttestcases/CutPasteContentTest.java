d63cfb4eff71f5950d10a4f59caccb2f63e26de6
luishernandezmonge <luis.hernandez@craftersoftware.com>
2017-11-27 14:43:41 -0600

    Added fix for CutPasteContentTest because currently it's stucked on assertion at the recent activity dashboard widget>> related to bug #1644 Modified some xpaths on shareUI properties

--- src/test/java/org/craftercms/studio/test/cases/contenttestcases/CutPasteContentTest.java
+++ src/test/java/org/craftercms/studio/test/cases/contenttestcases/CutPasteContentTest.java
@@ -44,26 +44,27 @@ public class CutPasteContentTest {
 	private String createFormFrameElementCss;
 	private String createFormSaveAndCloseElementId;
 	private String createFormMainTitleElementXPath;
-	private String testingItemURLXpath;
 	private String newFolderCreated;
-	private String dashboardMenuOption;
-	
-	
-	
+
+	private String newFolderSpanXpath;
+
+	private String testingItemURLXpath;
+
 	@BeforeClass
 	public void beforeTest() {
 		this.driverManager = new WebDriverManager();
 		UIElementsPropertiesManager UIElementsPropertiesManager = new UIElementsPropertiesManager(
 				FilesLocations.UIELEMENTSPROPERTIESFILEPATH);
-		ConstantsPropertiesManager constantsPropertiesManager = new ConstantsPropertiesManager(FilesLocations.CONSTANTSPROPERTIESFILEPATH);
-		
+		ConstantsPropertiesManager constantsPropertiesManager = new ConstantsPropertiesManager(
+				FilesLocations.CONSTANTSPROPERTIESFILEPATH);
+
 		this.driverManager.setConstantsPropertiesManager(constantsPropertiesManager);
-		
-		this.loginPage = new LoginPage(driverManager,UIElementsPropertiesManager);
+
+		this.loginPage = new LoginPage(driverManager, UIElementsPropertiesManager);
 		this.homePage = new HomePage(driverManager, UIElementsPropertiesManager);
 		this.dashboardPage = new DashboardPage(driverManager, UIElementsPropertiesManager);
 		this.siteConfigPage = new SiteConfigPage(driverManager, UIElementsPropertiesManager);
-		
+
 		userName = constantsPropertiesManager.getSharedExecutionConstants().getProperty("crafter.username");
 		password = constantsPropertiesManager.getSharedExecutionConstants().getProperty("crafter.password");
 		siteDropdownElementXPath = UIElementsPropertiesManager.getSharedUIElementsLocators()
@@ -82,11 +83,11 @@ public class CutPasteContentTest {
 		createFormMainTitleElementXPath = UIElementsPropertiesManager.getSharedUIElementsLocators()
 				.getProperty("general.createformTitle");
 		testingItemURLXpath = UIElementsPropertiesManager.getSharedUIElementsLocators()
-				.getProperty("general.myrecentactivity.firstelementurl");
+				.getProperty("general.cutpaste.pastedelement");
 		newFolderCreated = UIElementsPropertiesManager.getSharedUIElementsLocators()
 				.getProperty("dashboard.add_new_folder");
-		dashboardMenuOption = UIElementsPropertiesManager.getSharedUIElementsLocators()
-				.getProperty("dashboard.dashboard_menu_option");
+		newFolderSpanXpath = UIElementsPropertiesManager.getSharedUIElementsLocators()
+				.getProperty("general.sitecontent.newfolder");
 	}
 
 	@AfterClass
@@ -104,16 +105,15 @@ public class CutPasteContentTest {
 		// go to dashboard page
 
 		homePage.goToDashboardPage();
-		
+
 		driverManager.getDriver().navigate().refresh();
-		
+
 		// Show site content panel
-		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "xpath", siteDropdownElementXPath)
-				.click();
-		
+		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed("xpath", siteDropdownElementXPath).click();
+
 		// go to admin console page
-		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "xpath", adminConsoleXpath).click();
-		
+		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed("xpath", adminConsoleXpath).click();
+
 		// select content types
 		siteConfigPage.selectContentTypeOption();
 
@@ -125,27 +125,27 @@ public class CutPasteContentTest {
 		siteConfigPage.confirmContentTypeSelected();
 
 		// select main content
-		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "xpath", entryContentTypeBodyXpath).click();
-		
+		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed("xpath", entryContentTypeBodyXpath).click();
 
 		// Body not required
-		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "cssSelector",
-				entryContentTypeBodyCheckCss).click();
+		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed("cssSelector", entryContentTypeBodyCheckCss)
+				.click();
 
 		// save
 		siteConfigPage.saveDragAndDropProcess();
-		
+
 		// Switch to the form
 		driverManager.getDriver().navigate().refresh();
 		driverManager.getDriver().switchTo().defaultContent();
-		
+
 		// go to dashboard
-		//this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable( "id", studioLogo);
-		this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable( "id", studioLogo).click();
-		
+		// this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable(
+		// "id", studioLogo);
+		this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable("id", studioLogo).click();
+
 		// expand pages folder
 		dashboardPage.expandPagesTree();
-		
+
 		// right click to see the the menu
 		dashboardPage.rightClickToSeeMenu();
 
@@ -157,26 +157,27 @@ public class CutPasteContentTest {
 
 		// Switch to the iframe
 		driverManager.getDriver().switchTo().defaultContent();
-		driverManager.getDriver().switchTo().frame(this.driverManager.driverWaitUntilElementIsPresentAndDisplayed(
-				"cssSelector", createFormFrameElementCss));
+		driverManager.getDriver().switchTo().frame(this.driverManager
+				.driverWaitUntilElementIsPresentAndDisplayed("cssSelector", createFormFrameElementCss));
 
 		this.driverManager.isElementPresentBycssSelector(createFormFrameElementCss);
-		
+
 		// Set basics fields of the new content created
 		dashboardPage.setBasicFieldsOfNewContent("Test1", "Testing1");
 
 		// Set the title of main content
-		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "xpath",createFormMainTitleElementXPath)
+		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed("xpath", createFormMainTitleElementXPath)
 				.sendKeys("MainTitle");
-		
+
 		// save and close
-		this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable( "id", createFormSaveAndCloseElementId).click();
+		this.driverManager
+				.driverWaitUntilElementIsPresentAndDisplayedAndClickable("id", createFormSaveAndCloseElementId).click();
 		// save and close
-		
+
 		driverManager.getDriver().switchTo().defaultContent();
-		
-		//reload page
-        driverManager.getDriver().navigate().refresh();
+
+		// reload page
+		driverManager.getDriver().navigate().refresh();
 
 		// expand home content
 
@@ -193,44 +194,31 @@ public class CutPasteContentTest {
 		// Create folder button
 
 		dashboardPage.clickCreateButton();
-		
+
 		this.driverManager.driverWaitUntilElementIsPresentAndDisplayed("xpath", newFolderCreated);
-		
+
 		// Right click and cut content.
 		dashboardPage.rightClickToCutOption();
-		
+
 		// Right click and paste content.
 
 		dashboardPage.rightClickToPasteIntoFolder();
 
 		// reload page
+		driverManager.getDriver().navigate().refresh();
+		driverManager.getDriver().navigate().refresh();
 
-		driverManager.getDriver().navigate().refresh();
-		driverManager.getDriver().navigate().refresh();
+		this.driverManager.isElementPresentAndClickableByXpath(newFolderSpanXpath);
+		this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable("xpath", newFolderSpanXpath).click();
 		
-//		this.driverManager.isElementPresentByXpath(testingItemURLXpath);
-//		
-//		// Assert of the content copied
-//		this.driverManager.isElementPresentByXpath(testingItemURLXpath);
-		String contentCopied = this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "xpath",
-				testingItemURLXpath).getText();
+		this.driverManager.waitUntilPageLoad();
 		
-		while(!(contentCopied.contains("/addnewfolder/test1")))
-		{
-//			this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable(
-//					"xpath", dashboardMenuOption);
-			this.driverManager.driverWaitUntilElementIsPresentAndDisplayedAndClickable(
-					"xpath", dashboardMenuOption).click();
-//			this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "xpath",
-//					testingItemURLXpath);
-			 contentCopied = this.driverManager.driverWaitUntilElementIsPresentAndDisplayed( "xpath",
-					testingItemURLXpath).getText();
-		}
-		
-		Assert.assertEquals(contentCopied, "/addnewfolder/test1");
-	
+		// Assert of the content copied
+		this.driverManager.waitWhileElementIsDisplayedAndClickableByXpath(testingItemURLXpath);
+		String contentCopied = this.driverManager
+				.driverWaitUntilElementIsPresentAndDisplayed("xpath", testingItemURLXpath).getText();
+		Assert.assertEquals(contentCopied, "Testing1");
 
 	}
 
 }
-
