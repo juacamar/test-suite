15443bee96883c6a1ffc842ba48849bed85ca8dd
Christopher Lim <christopher.lim@rivetlogic.com>
2017-12-21 10:04:10 -0500

    Fixing errors in tests resulting from issue #1714
    Problems resolved in classes:
    GetLoggersAPITest
    MemoryAPITest
    SetLoggerStateAPITest
    StatusAPITest
    ValidateSessionAPITest
    VersionAPITest

--- src/test/java/org/craftercms/studio/test/cases/apitestcases/VersionAPITest.java
+++ src/test/java/org/craftercms/studio/test/cases/apitestcases/VersionAPITest.java
@@ -1,8 +1,11 @@
 package org.craftercms.studio.test.cases.apitestcases;
 
 import org.craftercms.studio.test.api.objects.MonitoringAPI;
+import org.craftercms.studio.test.api.objects.SecurityAPI;
 import org.craftercms.studio.test.utils.APIConnectionManager;
 import org.craftercms.studio.test.utils.JsonTester;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
@@ -12,17 +15,29 @@ import org.testng.annotations.Test;
 public class VersionAPITest {
 
     private MonitoringAPI monitoringAPI;
+    private SecurityAPI securityAPI;
     
     public VersionAPITest(){
     	APIConnectionManager apiConnectionManager = new APIConnectionManager();
 		JsonTester api = new JsonTester(apiConnectionManager.getProtocol(), apiConnectionManager.getHost(),
 				apiConnectionManager.getPort());
-    	
+		
+		securityAPI = new SecurityAPI(api, apiConnectionManager);
     	monitoringAPI = new MonitoringAPI(api, apiConnectionManager);
     }
 
+    @BeforeTest
+	public void beforeTest() {
+		securityAPI.logInIntoStudioUsingAPICall();
+	}
+    
     @Test(priority=1)
     public void testVersion(){
     	monitoringAPI.testVersion();
     }
+    
+    @AfterTest
+	public void afterTest() {
+		securityAPI.logOutFromStudioUsingAPICall();
+	}
 }
