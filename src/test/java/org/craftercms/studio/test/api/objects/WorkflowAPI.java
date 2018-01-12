08cfbe8c9743b5084756501b8003e78179f0ae57
Christopher Lim <christopher.lim@rivetlogic.com>
2018-01-12 16:03:04 -0500

    Workflow API
    Create Jobs
    
    also updating testing xml with GetUserActivity and CreateJobs

--- src/test/java/org/craftercms/studio/test/api/objects/WorkflowAPI.java
+++ src/test/java/org/craftercms/studio/test/api/objects/WorkflowAPI.java
@@ -1,5 +1,8 @@
 package org.craftercms.studio.test.api.objects;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.craftercms.studio.test.utils.APIConnectionManager;
 import org.craftercms.studio.test.utils.JsonTester;
 
@@ -7,5 +10,24 @@ public class WorkflowAPI extends BaseAPI{
 
 	public WorkflowAPI(JsonTester api, APIConnectionManager apiConnectionManager) {
 		super(api, apiConnectionManager);
+	}
+	
+	public void testCreateJobs(String siteId) {
+		
+		Map<String, Object> json = new HashMap<>();
+		json.put("sendEmail", "true");
+		json.put("schedule", "now");
+		json.put("submissionComment", "");
+		
+		String[] items = new String[1];
+		items[0] = "/site/website/index.xml";
+		json.put("items", items);
+		
+		api.post("/studio/api/1/services/api/1/workflow/create-jobs.json")
+		.urlParam("site", siteId)
+		.urlParam("user","admin")
+		.json(json)
+		.execute().status(200)
+		.debug();
 	}
 }
