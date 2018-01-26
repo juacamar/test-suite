b81f165553ac25bafc8af312960551348576a6b8
Christopher Lim <christopher.lim@rivetlogic.com>
2018-01-26 11:23:24 -0500

    removing unnecessary code for commit

--- src/test/java/org/craftercms/studio/test/api/objects/SiteManagementAPI.java
+++ src/test/java/org/craftercms/studio/test/api/objects/SiteManagementAPI.java
@@ -158,87 +158,17 @@ public class SiteManagementAPI extends BaseAPI {
 	
 	public void testCreateSiteGitRepo(String siteId) throws Exception {
 		
-		
-		// invoke the process, keeping a handle to it for later...
-	    // note that we pass the command and its params as String's in
-	    // the same String[]
-	    Process _p = Runtime.getRuntime().exec("mkdir ./craftercms_temp");
-	    int a = _p.waitFor();
-	    System.out.println("asdf a "+ a);
-	    
-        
-	    Process _p3 = Runtime.getRuntime().exec("cd ./craftercms_temp");
-	    int c = _p3.waitFor();
-	    System.out.println("asdf c "+ c);
-		
-	    //Process _p2 = Runtime.getRuntime().exec("git --bare init");
-	    
-	    String[] command = {"git", "--bare", "init"};
-        ProcessBuilder probuilder = new ProcessBuilder( command );
-        //You can set up your work directory
-        probuilder.directory(new File("./craftercms_temp"));
-
-        Process _p2 = probuilder.start();
-	    
-        //Read out dir output
-        InputStream is = _p2.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        System.out.printf("Output of running mkdir ./craftercms_temp is:\n");
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
-	    
-	    int b = _p2.waitFor();
-	    System.out.println("asdf b "+ b);
-	    
-		
+		//temp git repo creation 
 		Map<String, Object> json = new HashMap<>();
 		json.put("site_id", siteId);
 		json.put("description", description);
-		json.put("blueprint", "empty");
-		json.put("use_remote", "true");
-		json.put("remote_name", "origin");
-		json.put("remote_url", "./craftercms_temp/.git");
-		json.put("remote_username", "");
-		json.put("remote_password", "");
-		json.put("create_option", "push");
-		api.post("/studio/api/1/services/api/1/site/create.json").json(json).execute()
-				.debug();
+		json.put("blueprint", blueprint);
+		api.post("/studio/api/1/services/api/1/site/create.json").json(json).execute().status(201)
+				.header("Location",
+						is(headerLocationBase + "/studio/api/1/services/api/1/site/get.json?site_id=" + siteId))
+				.json("$.message", is("OK")).debug();
 		this.setSiteId(siteId);
-		
-	    String[] command2 = {"rm", "./craftercms_temp"};
-        probuilder = new ProcessBuilder( command2 );
-        //You can set up your work directory
-        probuilder.directory(new File("./"));
-
-        Process _p4 = probuilder.start();
-        
-        InputStream is2 = _p4.getInputStream();
-        InputStreamReader isr2 = new InputStreamReader(is2);
-        BufferedReader br2 = new BufferedReader(isr2);
-        String line2;
-        System.out.printf("Output of running del is:\n");
-        while ((line2 = br2.readLine()) != null) {
-            System.out.println(line2);
-        }
-        
-	    int d = _p4.waitFor();
-	    System.out.println("asdf d "+ d);
 	}
-	
-	/*
-	 * 
-	 *
-	  "blueprint" : "empty",
-      "use_remote" : true,		      "use_remote" : true,
-      "remote_name" : "upstream",		      "remote_name" : "upstream",
- -    "remote_url" : "https://github.com/craftercms/remoterepo.git"		 +    "remote_url" : "https://github.com/craftercms/remoterepo.git",
- -    "username" : "joe.bloggs"		 +    "remote_username" : "joe.bloggs",
- -    "password" : "SuperSecret$$587"		 +    "remote_password" : "SuperSecret$$587",
-      "create_option" : "push"		      "create_option" : "push"
-	 */
 	
 	public String getSiteId() {
 		return siteId;
